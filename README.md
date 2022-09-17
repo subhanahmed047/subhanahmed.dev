5fce06b8d75455448ac7e52e7f9dd20c2e402dd8
Subhan Ahmed <subhanahmed047@gmail.com>
2022-09-17 12:02:01 +1000

    feat(app): setup blog using docusaurus

---
+++ README.md
@@ -0,0 +1,3 @@
+# subhanahmed.dev
+
+Just a website to put togather my thoughts and learnings about the things I care about. It's built using [Docusaurus](https://docusaurus.io/)
---
+++ docusaurus.config.js
@@ -0,0 +1,74 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'subhanahmed.dev',
+  tagline: 'Writing about the things I care about',
+  url: 'https://subhanahmed.dev',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'subhanahmed047', // Usually your GitHub org/user name.
+  projectName: 'subhanahmed.dev', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'subhanahmed.dev',
+        // logo: {
+        //   alt: 'subhanahmed.dev Logo',
+        //   src: 'img/logo.svg',
+        // },
+        items: [
+          { to: '/blog', label: 'Blog', position: 'right' },
+          {
+            href: 'https://github.com/subhanahmed047',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} <a href="https://subhanahmed.dev">subhanahmed.dev</a>`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
